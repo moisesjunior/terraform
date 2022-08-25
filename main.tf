253b766104a37ccb010eafab27b198366623ff01
Moisés Junior <nem.moises@hotmail.com>
2022-08-25 19:57:34 -0300

    feat: creating default ec2 instance in AWS

---
+++ main.tf
@@ -0,0 +1,42 @@
+// AWS
+
+terraform {
+  cloud {
+    organization = "OsCria"
+    workspaces {
+      name = "estudoDosCria"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
